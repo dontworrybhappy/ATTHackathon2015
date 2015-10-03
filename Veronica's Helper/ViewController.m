efd98be33cc4b1185df3e1d13d78a2e60a4cea4d
phale01 <phale01@saintmarys.edu>
2015-10-02 22:58:53 -0400

    Initial Commit

---
+++ Veronica's Helper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Veronica's Helper
+//
+//  Created by Patricia "Happy" Hale on 10/2/15.
+//  Copyright © 2015 Patricia "Happy" Hale. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
